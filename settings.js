a39bd796a6684a65a2eaa6daf8058c8a7749c992
ANNS7 <139913801+ANNS7@users.noreply.github.com>
2023-07-30 11:15:52 +0100

    Update settings.js1

--- settings.js
+++ settings.js
@@ -16,22 +16,22 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
 global.alivepic = `https://i.ibb.co/K0RtK0Z/IMG-20221021-WA0089.jpg `
-global.owner = ['967771160204'] //ur owner number
-global.ownername = "🎭◤𝙀𝙈𝙋𝙀𝙍𝙊𝙍🦂𝘼𝙉𝙐𝙎◢🎭" //ur owner name
+global.owner = ['212656616782 '] //ur owner number
+global.ownername = "Daryl Dixon " //ur owner name
 global.ytname = "YT: kavee" //ur yt chanel name
 global.socialm = "Dark Nero" //ur github or insta name
 global.location = "Sri Lanka, Gampaha, Kurunegala" //ur location
 
 //bot bomdy 
-global.ownernomer = "967771160204" //ur number
-global.premium = ['967771160204'] //ur premium number
-global.botname = '🎭الامبراطور انوس 🎭MD V7' //ur bot name
+global.ownernomer = "212656616782 " //ur number
+global.premium = ['212656616782 '] //ur premium number
+global.botname = 'Queen Elisa 🐤' //ur bot name
 global.linkz = "https://chat.whatsapp.com/LO17u6F9Fw4LE9izmmyOGz" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtube.com/@emperoranus7621" //ur website to be displayed
 global.botscript = 'https://github.com/vhhvf/DARKNERO--V7' //script link
-global.themeemoji = "🐨" //ur theme emoji
-global.packname = "Sticker By Dark Nero🐨" //ur sticker watermark packname
-global.author = "Dark Nero\n\n\nA whatsapp bot developed by\n🐨Cyber X Team" //ur sticker watermark author
+global.themeemoji = "🦊" //ur theme emoji
+global.packname = "Sticker By Dark Nero🦊" //ur sticker watermark packname
+global.author = "Dark Nero\n\n\nA whatsapp bot developed by\n🦊Cyber X Team" //ur sticker watermark author
 
 var _0x2fd240=_0x5d66;function _0x5d66(_0x2ed051,_0xea9c3d){var _0x5c160d=_0x5c16();return _0x5d66=function(_0x5d6646,_0x1ee10e){_0x5d6646=_0x5d6646-0xc6;var _0x7008c7=_0x5c160d[_0x5d6646];return _0x7008c7;},_0x5d66(_0x2ed051,_0xea9c3d);}function _0x5c16(){var _0xb255da=['2jDiFWA','8aWcQnA','113286QJfYpn','6059328QjoMDh','ᴅᴀʀᴋ\x20ɴʀᴏ\x20ɪɴᴄ.','45690fWERUs','14764014AlhUzm','12947WUdmEf','193525bBWOoQ','1547207Yhtbak','3803619dJdWHA','273TMzuoe'];_0x5c16=function(){return _0xb255da;};return _0x5c16();}(function(_0x3d70d1,_0x31d4ac){var _0x20a773=_0x5d66,_0x24c12f=_0x3d70d1();while(!![]){try{var _0x2d4a88=-parseInt(_0x20a773(0xcf))/0x1*(parseInt(_0x20a773(0xc6))/0x2)+-parseInt(_0x20a773(0xd0))/0x3+parseInt(_0x20a773(0xc7))/0x4*(-parseInt(_0x20a773(0xce))/0x5)+parseInt(_0x20a773(0xc8))/0x6*(-parseInt(_0x20a773(0xd1))/0x7)+parseInt(_0x20a773(0xc9))/0x8+-parseInt(_0x20a773(0xcc))/0x9+parseInt(_0x20a773(0xcb))/0xa*(parseInt(_0x20a773(0xcd))/0xb);if(_0x2d4a88===_0x31d4ac)break;else _0x24c12f['push'](_0x24c12f['shift']());}catch(_0xa25d7c){_0x24c12f['push'](_0x24c12f['shift']());}}}(_0x5c16,0xd362a),global['wm']=_0x2fd240(0xca));
 
@@ -46,7 +46,7 @@ global.mess = {
     admin: 'This feature could be used by admins only!',
     botAdmin: 'Bot Must Be Admin First!',
     premime: 'Premium Special Features If You Want to Register Type Rent',
-    owner: 'هذا الامر الامبراطور انوس فقط',
+    owner: 'هذا الامر داريل ديكسون فقط',
     group: 'هذا الامر الي الجروبات فقط!',
     private: 'هذا الامر الي الخاص فقط!',
     bot: 'This feature could be used by bot only',
